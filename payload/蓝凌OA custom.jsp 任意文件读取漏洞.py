e6eb412128a92fc573113515a6fa51cbcc33a6e5
Lemon <51054495+bgbing@users.noreply.github.com>
2021-05-01 20:21:58 +0800

    Update 蓝凌OA custom.jsp 任意文件读取漏洞.py

--- payload/蓝凌OA custom.jsp 任意文件读取漏洞.py
+++ payload/蓝凌OA custom.jsp 任意文件读取漏洞.py
@@ -4,7 +4,7 @@ urllib3.disable_warnings()
 
 def poc():
     print('+---------------------------------------------------+')
-    print('+ \033[1;36m蓝凌OA custom.jsp 任意文件读取漏洞\033[0m                 +')
+    print('+ \033[1;36m蓝凌OA custom.jsp 任意文件读取漏洞\033[0m                +')
     print('+ \033[1;36m公众号：bgbing安全\033[0m                                +')
     print('+ \033[1;36m作者：黑子黑\033[0m                                      +')
     print('+ \033[1;36murl >>>http://xxx.xxx.xxx.xxx\033[0m                     +')
