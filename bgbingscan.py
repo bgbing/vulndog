f5c06e41e5133087462e8ec206b6be11b684512f
Lemon <51054495+bgbing@users.noreply.github.com>
2021-05-03 12:22:22 +0800

    Update bgbingscan.py

--- bgbingscan.py
+++ bgbingscan.py
@@ -21,6 +21,9 @@ if args.vuln!="":
     elif number==459721:
         ruijie='"锐捷云课堂主机目录遍历漏洞批量扫描.py"'
         os.system("cd payload &&"+ruijie)
+    elif number==19970:
+        ruijie='"安美数字 酒店宽带运营系统 server_ping.php 远程命令执行漏洞.py"'
+        os.system("cd payload &&"+ruijie)
 elif args.url!="":
     url=args.url
     all='"allpayload.py"'
@@ -47,6 +50,7 @@ else:
     print('\033[1;36m           python3 bgbingscan.py --vuln xxxx\033[0m')
     print("\n")
     print('\033[1;36m目前更新漏洞\033[0m')
+    print(' \033[1;36m          2021/5/1 安美数字 远程命令执行漏洞 (19970)\033[0m ')
     print(' \033[1;36m          2021/5/1 蓝凌OA custom.jsp 任意文件读取漏洞 (13579)\033[0m ')
     print(' \033[1;36m          2021/5/1 蓝凌OA custom.jsp 任意文件读取漏洞批量扫描 (135791)\033[0m ')
     print(' \033[1;36m          2021/5/1 锐捷云课堂主机目录遍历漏洞 (45972)\033[0m ')
